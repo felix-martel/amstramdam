ed0ae973e38b0dbe15a62f35225f7c545a16b89b
felix-martel <felix.martel@polytechnique.edu>
2020-11-17 22:59:55 +0100

    Create a file for constants (wip)

---
+++ front/common/constants.js
@@ -0,0 +1,13 @@
+export default {
+    colors: {
+        TRUE: "blue",
+        SELF: "red",
+        BASE: "purple"
+    },
+    animations: {
+        map: {
+            duration: 0.5,
+            deltaPad: 0.5,
+        },
+    },
+}
