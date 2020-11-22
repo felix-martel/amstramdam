13a258a7beb6461c41e99e5daab867eb7ba4e8c5
felix-martel <felix.martel@polytechnique.edu>
2020-11-22 11:48:01 +0100

    Fix margin issue with run indicator

--- front/panels/hintBox/hintBox.vue
+++ front/panels/hintBox/hintBox.vue
@@ -1,22 +1,13 @@
 <template>
   <div class="game-info box" id="display-hint">
-                <span class="run-info">
-                    <span id="run-current">{{ currentRun }}</span>
-                  /
-                  <span id="run-total">{{ totalRuns }}</span>
-                </span>
+    <span class="run-info">
+        <span id="run-current">{{ currentRun }}</span>
+      /
+      <span id="run-total">{{ totalRuns }}</span>
+    </span>
     <span id="target">
       {{ hint }}
     </span>
-<!--    <div class="progress-container">-->
-<!--      <div class="progress-inner">-->
-<!--        <div class="wrapper" id="countdown-animation-wrapper" data-anim="base wrapper">-->
-<!--          <div class="circle" id="countdown-animation-left" data-anim="base left"></div>-->
-<!--          <div class="circle" id="countdown-animation-right" data-anim="base right"></div>-->
-<!--        </div>-->
-
-<!--      </div>-->
-<!--    </div>-->
     <radial-countdown :duration="runDuration"></radial-countdown>
   </div>
 </template>
@@ -50,4 +41,7 @@ export default {
 </script>
 
 <style scoped>
+.run-info {
+  margin-right: 10px;
+}
 </style>
