e974d41d237fb1db9d46778afe762fb50da3d420
felix-martel <felix.martel@polytechnique.edu>
2020-11-23 11:12:02 +0100

    Moved style from 'style.css' to component file

--- front/ui/footer.vue
+++ front/ui/footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="game-sharing-link">Inviter des amis: <u id="sharing-link">{{ url }}</u>
+  <div class="footer" id="game-sharing-link">
+    <span class="sharing">
+      Inviter des amis: <u class="sharing-link">{{ url }}</u>
+    </span>
     <span class="option-bar">
         <input type="checkbox" name="autozoom" v-model="autozoomHandler.state" id="autozoom-check">
         <label for="autozoom-check">Autozoom</label>
@@ -36,3 +39,25 @@
   }
 
 </script>
+
+<style scoped>
+  .footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: blue;
+    color: white;
+    z-index: 1000;
+    padding: 2px 10px;
+    font-size: 0.9em;
+  }
+
+  .footer .sharing-link {
+    color: white;
+    margin-right: 15px;
+    font-size: 1.1em;
+    font-weight: bold;
+  }
+
+
+</style>
