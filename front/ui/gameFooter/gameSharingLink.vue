780307d4fcc34873cc762f40854ed14aeafafc00
felix-martel <felix.martel@polytechnique.edu>
2020-11-27 16:09:40 +0100

    Use footer-collapsible-section in game-sharing-link

--- front/ui/gameFooter/gameSharingLink.vue
+++ front/ui/gameFooter/gameSharingLink.vue
@@ -1,21 +1,26 @@
 <template>
-  <span class="sharing">
-    <span class="sharer" @click="toggleView">
+    <footer-collapsible-section :collapsed="collapsed">
+    <template v-slot:control>
+      <span class="sharer" @click="toggleView">
       {{ message }}
     </span>
-    <a ref="link"
-       :class="{collapsed: collapsed, copied: copied}"
-       :style="{maxWidth: collapsed ? 0 : width}"
+    </template>
+    <template v-slot:content>
+      <a ref="link"
+       :class="{copied: copied}"
        class="sharing-link"
        :href="trueUrl"
        @click.prevent="copyUrlToClipboard">{{ trueUrl }}</a>
-  </span>
+    </template>
+  </footer-collapsible-section>
 </template>
 
 <script>
-// TODO: use footerCOllapsibleSection
-
+import footerCollapsibleSection from "./footerCollapsibleSection.vue";
 export default {
+  components: {
+    "footer-collapsible-section": footerCollapsibleSection,
+  },
   data() {
     const trueUrl = window.location.href
     const url = trueUrl.replace("https://", "")
@@ -25,14 +30,9 @@ export default {
       url: url,
       trueUrl: trueUrl,
       collapsed: true,
-      width: 0,
       timeout: undefined,
       copied: false,
     }
-  },
-  mounted() {
-    this.width = this.getIntrinsicWidth(this.$refs.link);
-
   },
   methods: {
     copyUrlToClipboard() {
@@ -85,16 +85,6 @@ export default {
         this.hideLink();
       }
     },
-
-    getIntrinsicWidth(el) {
-      if (!el) return -1;
-      el.style.maxWidth = "none";
-      const width = getComputedStyle(el).width;
-      el.style.display = "inline-block";
-      el.style.maxWidth = 0;
-      console.log("Computed width:", width);
-      return width;
-    }
   },
 
   computed: {
@@ -113,26 +103,18 @@ export default {
 }
 a.sharing-link {
   color: white !important;
-  /*margin-right: 15px;*/
   font-size: 1.1em;
   font-weight: bold;
-  transition: all 0.4s ease;
   display: inline-block;
-  overflow-x: hidden;
-  overflow-y: visible;
   white-space: nowrap;
   vertical-align: bottom;
-  box-sizing: border-box;
 }
 a.sharing-link:hover {
   color: white !important;
 }
 
-a.sharing-link.collapsed {
-  opacity: 0;
-}
 
-a.sharing-link:not(.collapsed):after {
+div:not(.collapsed) a.sharing-link:after {
   display: block;
   content: "Lien copié !";
   background-color: black;
@@ -145,7 +127,7 @@ a.sharing-link:not(.collapsed):after {
   transition: all 0.2s ease-in;
 }
 
-a.sharing-link.copied:after {
+div:not(.collapsed) a.sharing-link.copied:after {
   top: -25px;
   opacity: 1;
   transition: all 0.2s ease-out;
