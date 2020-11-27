62614fa4770389c8c4beba96d134e1adfc5ad111
felix-martel <felix.martel@polytechnique.edu>
2020-11-27 15:32:40 +0100

    Enhanced game sharing link (hidden by default, less confusing labels, auto-copy to clipboard)

---
+++ front/ui/gameFooter/footer.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="footer" id="game-sharing-link">
+    <game-sharing-link></game-sharing-link>
+    <span class="option-bar">
+        <input type="checkbox" name="autozoom" v-model="autozoomHandler.state" id="autozoom-check">
+        <label for="autozoom-check">Autozoom</label>
+    </span>
+  </div>
+</template>
+<script>
+  import {CookieHandler, BooleanCookieHandler, BooleanSettingHandler} from "../../common/cookie.js";
+  import gameSharingLink from "./gameSharingLink.vue";
+  export default {
+    components: {
+      "game-sharing-link": gameSharingLink,
+    },
+    data () {
+      return {
+        autozoomHandler: new BooleanSettingHandler("autozoom", this.$store),
+        url: ""
+      }
+    },
+
+    computed: {
+    },
+
+    created() {
+      this.url = window.location.href
+          .replace("https://", "")
+          .replace("www.", "")
+          .replace(window.location.hash, "");
+    },
+
+    methods: {
+      toggleAutozoom() {
+        let value = this.autozoomHandler.read();
+        this.autozoomHandler.write(!value);
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background: blue;
+    color: white;
+    z-index: 1000;
+    padding: 2px 10px;
+    font-size: 0.9em;
+  }
+
+  .footer .sharing-link {
+    color: white;
+    margin-right: 15px;
+    font-size: 1.1em;
+    font-weight: bold;
+  }
+
+
+</style>
---
+++ front/ui/gameFooter/gameSharingLink.vue
@@ -0,0 +1,151 @@
+<template>
+  <span class="sharing">
+    <span class="sharer" @click="toggleView">
+      {{ message }}
+    </span>
+    <a ref="link"
+       :class="{collapsed: collapsed, copied: copied}"
+       :style="{maxWidth: collapsed ? 0 : width}"
+       class="sharing-link"
+       :href="trueUrl"
+       @click.prevent="copyUrlToClipboard">{{ trueUrl }}</a>
+  </span>
+</template>
+
+<script>
+export default {
+  data() {
+    const trueUrl = window.location.href
+    const url = trueUrl.replace("https://", "")
+          .replace("www.", "")
+          .replace(window.location.hash, "");
+    return {
+      url: url,
+      trueUrl: trueUrl,
+      collapsed: true,
+      width: 0,
+      timeout: undefined,
+      copied: false,
+    }
+  },
+  mounted() {
+    this.width = this.getIntrinsicWidth(this.$refs.link);
+
+  },
+  methods: {
+    copyUrlToClipboard() {
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(this.$refs.link);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      // this.$refs.link.select();
+      document.execCommand("copy");
+      this.copied = true;
+      window.setTimeout(() => {
+        this.copied = false;
+      }, 2*1000);
+      this.timeout = this.startTimeout(() => {
+        this.hideLink();
+      }, 3*1000);
+    },
+
+    clearTimeout() {
+      if (typeof this.timeout !== "undefined") {
+        window.clearTimeout(this.timeout);
+        this.timeout = "undefined";
+      }
+    },
+
+    startTimeout(handler, duration) {
+      this.clearTimeout();
+      this.timeout = window.setTimeout(handler, duration);
+    },
+
+    showLink() {
+      this.clearTimeout();
+      this.collapsed = false;
+    },
+
+    hideLink() {
+      this.collapsed = true;
+      this.copied = false;
+      this.clearTimeout();
+    },
+
+    toggleView() {
+      if (this.collapsed){
+        this.showLink();
+        this.timeout = this.startTimeout(() => {
+          this.hideLink();
+        }, 20*1000);
+      } else {
+        this.hideLink();
+      }
+    },
+
+    getIntrinsicWidth(el) {
+      if (!el) return -1;
+      el.style.maxWidth = "none";
+      const width = getComputedStyle(el).width;
+      el.style.display = "inline-block";
+      el.style.maxWidth = 0;
+      console.log("Computed width:", width);
+      return width;
+    }
+  },
+
+  computed: {
+    message() {
+      return this.collapsed ? "Inviter des amis" : "Lien à partager : ";
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sharer {
+  cursor: pointer;
+  display: inline-block;
+  margin-right: 10px;
+}
+a.sharing-link {
+  color: white !important;
+  /*margin-right: 15px;*/
+  font-size: 1.1em;
+  font-weight: bold;
+  transition: all 0.4s ease;
+  display: inline-block;
+  overflow-x: hidden;
+  overflow-y: visible;
+  white-space: nowrap;
+  vertical-align: bottom;
+  box-sizing: border-box;
+}
+a.sharing-link:hover {
+  color: white !important;
+}
+
+a.sharing-link.collapsed {
+  opacity: 0;
+}
+
+a.sharing-link:not(.collapsed):after {
+  display: block;
+  content: "Lien copié !";
+  background-color: black;
+  padding: 5px 10px;
+  position: absolute;
+  top: -10px;
+  right: 50%;
+  opacity: 0;
+  font-size: 0.8em;
+  transition: all 0.2s ease-in;
+}
+
+a.sharing-link.copied:after {
+  top: -25px;
+  opacity: 1;
+  transition: all 0.2s ease-out;
+}
+</style>
