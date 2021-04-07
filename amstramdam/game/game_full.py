c5ed1429d7b46b43243ee2b64020bd2480c3fa95
felix-martel <felix.martel@polytechnique.edu>
2021-04-07 21:13:19 +0200

    Add type hints to ams.game.game_full

--- amstramdam/game/game_full.py
+++ amstramdam/game/game_full.py
@@ -1,63 +1,107 @@
+from typing import Any, Iterable, Optional, Literal, Union
 import string
 from collections import defaultdict, Counter
 
 import pandas as pd
 import numpy as np
 
-from .game import GameRun, load_cities
+from .game import GameRun, load_cities, PlaceToGuess
 from ..datasets import dataloader
 from datetime import datetime, timedelta
 import random
 import amstramdam.game.status as status
+from amstramdam.datasets.game_map import GameMap
+from .types import (
+    Player,
+    Pseudo,
+    GameParams,
+    PlaceDict,
+    Record,
+    GameMetrics,
+    FullGameRunParams,
+    GameFinalResults,
+    MetricSummary,
+    PlayerFinalResult,
+    Leaderboard,
+)
 
 with open("data/player_names.txt", "r", encoding="utf8", errors="ignore") as f:
-    NAMES = {line.rstrip() for line in f}
+    NAMES: set[Pseudo] = {Pseudo(line.rstrip()) for line in f}
 
 
-def choose_countries(df, max_per_country=10, max_rank=1000):
-    countries = Counter()
+def choose_countries(
+    df: pd.DataFrame, max_per_country: int = 10, max_rank: int = 1000
+) -> pd.DataFrame:
+    countries: Counter[str] = Counter()
     cities = []
     for i, row in df.iterrows():
         country = row["country"]
-        if country not in countries or (i < max_rank and countries[country] < max_per_country):
+        if country not in countries or (
+            i < max_rank and countries[country] < max_per_country
+        ):
             countries[country] += 1
-            cities.append({k: row[k] for k in ["name", "country", "lat", "lon", "population"]})
+            cities.append(
+                {k: row[k] for k in ["name", "country", "lat", "lon", "population"]}
+            )
     return pd.DataFrame(cities)
 
 
-def choose_depts(df, max_per_country=10, min_pop=15):
+def choose_depts(
+    df: pd.DataFrame, max_per_country: int = 10, min_pop: int = 15
+) -> pd.DataFrame:
     data = df.to_dict("records")
-    countries = Counter()
+    countries: Counter[str] = Counter()
     cities = []
     for row in sorted(data, key=lambda p: -p["population"]):
         country = row["dept"]
-        if country not in countries or (min_pop < row["population"] and countries[country] < max_per_country):
+        if country not in countries or (
+            min_pop < row["population"] and countries[country] < max_per_country
+        ):
             countries[country] += 1
-            cities.append({k: row[k] for k in ["name", "dept", "lat", "lon", "population"]})
+            cities.append(
+                {k: row[k] for k in ["name", "dept", "lat", "lon", "population"]}
+            )
     return pd.DataFrame(cities)
 
 
-available_names = set(NAMES)
-global_player_list = set()
+available_names: set[Pseudo] = set(NAMES)
+global_player_list: set[Player] = set()
 
 
-def get_cities(map):
+def get_cities(map: dict[str, Any]) -> Iterable[PlaceToGuess]:
     return load_cities(map["fname"], map["min-pop"])
 
 
 class Game:
-    def __init__(self, name, players=None, n_run=20, time_param=5, dist_param=None,
-                 is_permanent=False, precision_mode=False,
-                 difficulty=1, is_public=False, creation_date=None, allow_zoom=False,
-                 duration=10, wait_time=8, map="world", pseudos=None, **kwargs):
+    def __init__(
+        self,
+        name: str,
+        players: Optional[set[Player]] = None,
+        n_run: int = 20,
+        time_param: int = 5,
+        dist_param: Optional[int] = None,
+        is_permanent: bool = False,
+        precision_mode: bool = False,
+        difficulty: int = 1,
+        is_public: bool = False,
+        creation_date: Optional[datetime] = None,
+        allow_zoom: bool = False,
+        duration: int = 10,
+        wait_time: int = 8,
+        map: str = "world",
+        pseudos: Optional[dict[Player, Pseudo]] = None,
+        # **kwargs,
+    ) -> None:
         self.name = name
         self.map_name = map
-        map = dataloader.load(self.map_name, difficulty)  # GameMap.from_name(self.map_name)
-        self.map_display_name = map.name
+        game_map: GameMap = dataloader.load(
+            self.map_name, difficulty
+        )  # GameMap.from_name(self.map_name)
+        self.map_display_name = game_map.name
         self.is_permanent = is_permanent
         if dist_param is None:
-            dist_param = map.distance
-        self.bbox = map.bbox
+            dist_param = game_map.distance
+        self.bbox = game_map.bbox
         # self.map_info = map
         self.difficulty = difficulty
         self.n_run = n_run
@@ -65,55 +109,70 @@ class Game:
         self.time_param = time_param
         self.precision_mode = precision_mode
         self.small_scale = (
-                    dist_param < 15)  # When the characteristic distance is below 15km, the game is considered 'small-scale'
+            dist_param < 15
+        )  # When the characteristic distance is below 15km, the game is considered 'small-scale'
         self.__curr_run_id = 0
         self.allow_zoom = allow_zoom
         if players is None:
             players = set()
         self.players = set(players)
-        self.places = map.sample(self.n_run)  # random.sample(get_cities(map), self.n_run)
+        self.places = game_map.sample(
+            self.n_run
+        )  # random.sample(get_cities(map), self.n_run)
         self.duration = duration
-        self.runs = [GameRun(self.players, place, **self.get_run_params())
-                     for place in self.places]
-        names = list(NAMES - self.players)
-        random.shuffle(names)
+        self.runs = [
+            GameRun(place=place, **self.get_run_params()) for place in self.places
+        ]
+        # names = list(NAMES - self.players)
+        # random.shuffle(names)
         self.global_player_list = global_player_list
-        self.available_names = available_names  # set(names)
+        self.available_names = available_names.copy()  # set(names)
         self.wait_time = wait_time
-        self.records = []  # defaultdict(list)
-        self.scores = defaultdict(int)
+        self.records: list[list[Record]] = []  # defaultdict(list)
+        self.scores: defaultdict[Player, int] = defaultdict(int)
         self.is_public = is_public
         if pseudos is None:
             pseudos = dict()
         pseudos = {k: v for k, v in pseudos.items() if k in self.players}
         self.done = False
         self.pseudos = pseudos
-        self.metrics = dict(distance=defaultdict(list), delay=defaultdict(list))
+        self.metrics: GameMetrics = dict(
+            distance=defaultdict(list), delay=defaultdict(list)
+        )
 
         if creation_date is None:
             creation_date = datetime.now()
-        self.date_created = creation_date
-        self.launched = False
+        self.date_created: datetime = creation_date
+        self.launched: bool = False
         self.status = status.NOT_LAUNCHED
         self.__id_counter = len(self.players)
         # self.run_in_progress = False
 
-    def get_new_id(self):
-        return ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=16))
+    def get_new_id(self) -> str:
+        return "".join(
+            random.choices(
+                string.ascii_lowercase + string.ascii_uppercase + string.digits, k=16
+            )
+        )
         # curr_id = self.__id_counter
         # self.__id_counter += 1
         # return str(curr_id)
 
-    def generate_player_name(self):
-        return f"{self.name}_{self.get_new_id()}"
+    def generate_player_name(self) -> Player:
+        return Player(f"{self.name}_{self.get_new_id()}")
 
-    def get_run_params(self):
-        return dict(dist_param=self.dist_param, time_param=self.time_param,
-                    precision_mode=self.precision_mode,
-                    duration=self.duration, non_linear=not self.allow_zoom)
+    def get_run_params(self) -> FullGameRunParams:
+        return FullGameRunParams(
+            players=self.players,
+            dist_param=self.dist_param,
+            time_param=self.time_param,
+            precision_mode=self.precision_mode,
+            duration=self.duration,
+            non_linear=not self.allow_zoom,
+        )
 
-    def get_params(self):
-        return dict(
+    def get_params(self) -> GameParams:
+        return GameParams(
             players=set(self.players),
             n_run=self.n_run,
             time_param=self.time_param,
@@ -132,17 +191,17 @@ class Game:
         )
 
     @property
-    def curr_run_id(self):
+    def curr_run_id(self) -> int:
         return self.__curr_run_id
 
     @curr_run_id.setter
-    def curr_run_id(self, value):
+    def curr_run_id(self, value: int) -> None:
         if value >= len(self.runs):
             value = len(self.runs) - 1
             self.done = True
         self.__curr_run_id = value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"""---
 Amstramdam {'Public' if self.is_public else 'Private'} Game
 Map: {self.map_display_name}
@@ -152,25 +211,30 @@ Places: {', '.join([p[0][0] for p in self.places])}
 Run: {self.curr_run_id + 1}/{self.n_run}
 ---"""
 
-    def generate_new_pseudo(self):
+    def generate_new_pseudo(self) -> Pseudo:
         if self.available_names:
-            return self.available_names.pop()
+            pseudo = self.available_names.pop()
         else:
-            return random.choice(list(NAMES))
+            pseudo = random.choice(list(NAMES))
+        return Pseudo(pseudo)
 
-    def print_pseudos(self):
+    def print_pseudos(self) -> str:
         pseudos = []
         for player in self.players:
             pseudo = self.get_pseudo(player)
-            if pseudo and pseudo != player:
+            if pseudo and str(pseudo) != str(player):
                 pseudos.append(f"{player} ({pseudo})")
             else:
                 pseudos.append(player)
         return ", ".join(pseudos)
 
-    def add_player(self, name=None, pseudo=None):
+    def add_player(
+        self, name: Optional[Player] = None, pseudo: Optional[Pseudo] = None
+    ) -> tuple[Player, Pseudo]:
         if name is not None:
-            assert name not in self.global_player_list and name not in self.players, f"Name '{name}' already exists"
+            assert (
+                name not in self.global_player_list and name not in self.players
+            ), f"Name '{name}' already exists"
             # assert name not in self.players, f"Name '{name}' already exists"
         else:
             name = self.generate_player_name()
@@ -184,54 +248,54 @@ Run: {self.curr_run_id + 1}/{self.n_run}
 
         return name, pseudo
 
-    def add_pseudo(self, name, pseudo):
+    def add_pseudo(self, name: Player, pseudo: Pseudo) -> None:
         if name not in self.players:
             print(f"Ignored unknown player '{name}'")
         self.pseudos[name] = pseudo
 
-    def request_pseudo(self, name):
+    def request_pseudo(self, name: Player) -> Pseudo:
         pseudo = self.generate_new_pseudo()
         self.add_pseudo(name, pseudo)
         return pseudo
 
-    def remove_pseudo(self, name):
+    def remove_pseudo(self, name: Player) -> None:
         if name in self.pseudos:
             del self.pseudos[name]
 
-    def get_pseudo(self, name):
-        return self.pseudos.get(name, name)
+    def get_pseudo(self, name: Player) -> Pseudo:
+        return self.pseudos.get(name, Pseudo(name))
 
-    def remove_player(self, name):
+    def remove_player(self, name: Player) -> None:
         if name in self.global_player_list:
             self.global_player_list.remove(name)
 
         if name not in self.players:
             return
-        if name in NAMES:
-            self.available_names.add(name)
+        # if name in NAMES:
+        #     self.available_names.add(name)
         self.players.remove(name)
         self.remove_pseudo(name)
-        if name in self.records:
-            del self.records[name]
+        # if name in self.records:
+        #     del self.records[name]
         if name in self.scores:
             del self.scores[name]
 
     @property
-    def current(self):
+    def current(self) -> GameRun:
         # if self.done:
         #     return self.runs[-1]
         return self.runs[self.curr_run_id]
 
-    def launch(self):
+    def launch(self) -> GameRun:
         self.launched = True
         self.status = status.LAUNCHING
         return self.current  # .launch()
 
-    def launch_run(self, *args, **kwargs):
+    def launch_run(self, *args: Any, **kwargs: Any) -> str:
         self.status = status.RUNNING
         return self.current.launch(*args, **kwargs)
 
-    def is_expired(self, hours=6):
+    def is_expired(self, hours: int = 6) -> bool:
         """Kwargs must be valid arguments for timedelta"""
         if self.is_permanent:
             return False
@@ -239,17 +303,19 @@ Run: {self.curr_run_id + 1}/{self.n_run}
         return len(self.players) == 0 and expiration_date < datetime.now()
 
     @property
-    def old_done(self):
+    def old_done(self) -> bool:
         return self.curr_run_id >= self.n_run
 
-    def on_game_end(self):
+    def on_game_end(self) -> None:
         print("Game ended!")
 
-    def places_as_json(self):
-        return [dict(location=place[0], hint=place[1], lon=point.lon, lat=point.lat) for place, point in self.places]
+    def places_as_json(self) -> list[PlaceDict]:
+        return [
+            PlaceDict(location=place[0], hint=place[1], lon=point.lon, lat=point.lat)
+            for place, point in self.places
+        ]
 
-    def get_final_results(self):
-        results = dict()
+    def get_final_results(self) -> GameFinalResults:
         distances = defaultdict(list)
         durations = defaultdict(list)
         for recs in self.records:
@@ -258,26 +324,17 @@ Run: {self.curr_run_id + 1}/{self.n_run}
                 distances[player].append(rec["dist"])
                 durations[player].append(rec["delta"])
 
-        def summary(values):
-            if not values:
-                return "-"
-            return np.median(values)
-
-        results = [dict(
-                player=player,
-                dist=summary(distances.get(player, [])),
-                delta=summary(durations.get(player, [])),
-                score=self.scores.get(player, 0)
-            ) for player in sorted(self.players, key=lambda p: -self.scores.get(p, 0))]
-
-        final_results = dict(records=self.get_filtered_records(),
-                             # scores=self.scores,
-                             places=self.places_as_json(),
-                             # summary=results
-                             )
+        final_results = GameFinalResults(
+            records=self.get_filtered_records(),
+            # scores=self.scores,
+            places=self.places_as_json(),
+            # summary=results
+        )
         return final_results
 
-    def get_filtered_records(self, keys=None):
+    def get_filtered_records(
+        self, keys: Optional[list[str]] = None
+    ) -> list[list[dict[str, Any]]]:
         """
         `self.records` contains a lot of information. This function filters it by keeping only the keys specified in
         `keys`. If not provided, `keys = ["guess", "player"]`, which are necessary for the final summary display.
@@ -298,14 +355,14 @@ Run: {self.curr_run_id + 1}/{self.n_run}
         records = [[{k: rec[k] for k in keys} for rec in recs] for recs in self.records]
         return records
 
-    def add_run_records(self, recs):
+    def add_run_records(self, recs: list[Record]) -> None:
         self.records.append(recs)
         for rec in recs:
             player = rec["player"]
             self.metrics["distance"][player].append(rec["dist"])
             self.metrics["delay"][player].append(rec["delta"])
 
-    def end(self):
+    def end(self) -> tuple[Union[GameRun, GameFinalResults], bool]:
         self.status = status.CORRECTION
 
         recs = self.current.records
@@ -318,69 +375,40 @@ Run: {self.curr_run_id + 1}/{self.n_run}
             self.on_game_end()
             results = self.get_final_results()
             return results, self.done
-        # self.current_run.launch()
         return self.current, self.done
 
-    def terminate(self):
+    def terminate(self) -> None:
         self.status = status.FINISHED
 
-    #
-    #
-    # def get_state_as_json(self):
-    #     s = self.status
-    #     game = dict(
-    #         name=self.map_name,
-    #         runs=self.n_run,
-    #         current=self.curr_run_id,
-    #         diff=self.difficulty
-    #     )
-    #     if s == status.NOT_LAUNCHED:
-    #         # 'init'
-    #         d = dict(
-    #             game=game,
-    #             pseudos=self.pseudos
-    #         )
-    #     elif s == status.LAUNCHING:
-    #         d = dict(
-    #             game=game
-    #         )
-    #     elif s == status.RUNNING:
-    #         d = dict(
-    #             hint=self.current.display(),
-    #             game=game
-    #         )
-    #     elif s == status.CORRECTION:
-    #         d = dict(
-    #             game=game,
-    #             answer=dict(
-    #
-    #             )
-    #         )
-
-    def safe_median(self, values):
+    def safe_median(self, values: Iterable[float]) -> MetricSummary:
         if not values:
             return "-"
-        return np.median(values)
+        median: float = np.median(values)
+        return median
 
-    def avg_distance(self, player):
+    def avg_distance(self, player: Player) -> MetricSummary:
         return self.safe_median(self.metrics["distance"].get(player, []))
 
-    def avg_delta(self, player):
+    def avg_delta(self, player: Player) -> MetricSummary:
         return self.safe_median(self.metrics["delay"].get(player, []))
 
     @property
-    def n_players(self):
+    def n_players(self) -> int:
         return len(self.players)
 
-    def get_player_score(self, player):
-        return dict(
+    def get_player_score(self, player: Player) -> PlayerFinalResult:
+        return PlayerFinalResult(
             player=player,
             score=self.scores[player],
             dist=self.avg_distance(player),
-            delta=self.avg_delta(player)
+            delta=self.avg_delta(player),
         )
 
-    def get_current_leaderboard(self):
+    def get_current_leaderboard(self) -> Leaderboard:
         """Players with their scores, ranked by decreasing scores"""
-        return list(sorted([self.get_player_score(player) for player in self.players],
-                           key=lambda t: -t["score"]))
+        return list(
+            sorted(
+                [self.get_player_score(player) for player in self.players],
+                key=lambda t: -t["score"],
+            )
+        )
